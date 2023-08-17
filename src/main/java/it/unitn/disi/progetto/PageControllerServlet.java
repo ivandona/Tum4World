3ba86e151a72c68c3fb2c76352340bffd4284a30
ivan <ivan.dona@studenti.unitn.it>
2023-08-17 10:51:33 +0200

    Initial commit

---
+++ src/main/java/it/unitn/disi/progetto/PageControllerServlet.java
@@ -0,0 +1,39 @@
+package it.unitn.disi.progetto;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PageControllerServlet", value = "/pageControllerServlet")
+public class PageControllerServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String page = request.getParameter("page"); // Get the requested page parameter
+
+        switch (page) {
+            case "home":
+                request.getRequestDispatcher("/WEB-INF/home.jsp").forward(request, response);
+                break;
+            case "simpatizzante":
+                request.getRequestDispatcher("/WEB-INF/simpatizzante.jsp").forward(request, response);
+                break;
+            case "aderente":
+                request.getRequestDispatcher("/WEB-INF/aderente.jsp").forward(request, response);
+                break;
+            case "admin":
+                request.getRequestDispatcher("/WEB-INF/admin.jsp").forward(request, response);
+                break;
+            case "info-cookie":
+                request.getRequestDispatcher("/WEB-INF/info-cookie.jsp").forward(request, response);
+                break;
+            default:
+                response.sendRedirect(request.getContextPath() + "/page?page=index");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
