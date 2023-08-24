ae57b01c30bf43c2ac8158679c7fa16c64c75e53
MattiaBress001 <mattia.bressan321@gmail.com>
2023-08-24 19:10:56 +0200

    Aggiunto il frontend

---
+++ src/main/java/it/unitn/disi/progetto/controllers/PublicPageController.java
@@ -0,0 +1,23 @@
+package it.unitn.disi.progetto.controllers;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import javax.servlet.annotation.*;
+import java.io.IOException;
+
+@WebServlet(name = "PublicPageController", value = "/publicPageController")
+public class PublicPageController extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String page = request.getParameter("page"); // Get the requested page parameter
+        String url = "/WEB-INF/" + page + ".jsp";
+
+        request.getRequestDispatcher(url).forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
