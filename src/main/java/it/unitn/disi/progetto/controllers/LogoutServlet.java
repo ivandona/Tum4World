19acbd9b1e2d36a46edb6ae75f9745f3757b8b6f
ivan <ivan.dona@studenti.unitn.it>
2023-08-25 21:17:10 +0200

    Fixata servlet di logout

--- src/main/java/it/unitn/disi/progetto/controllers/LogoutServlet.java
+++ src/main/java/it/unitn/disi/progetto/controllers/LogoutServlet.java
@@ -9,7 +9,7 @@ import java.io.IOException;
 public class LogoutServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        CookieController.invalidateCookiesAndSession(request, response);
+        CookieController.deleteAllCookiesAndAttributes(request, response);
         response.sendRedirect("index.jsp");
     }
 
